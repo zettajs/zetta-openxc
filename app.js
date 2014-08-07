f5e28f27bfef7deef684d827508d1d44cb6559af
Matt Dobson <mdobson4@gmail.com>
2014-08-07 11:08:51 -0400

    Updates to app to account for brake, wheel, and acceleration data streams. Also have a throttling mechanism to prevent multiple transition calls on stale stream data.

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 module.exports = function(server) {
   var vehicleQuery = server.where({type: 'vehicle'});
-  var hubQuery = server.where({type: 'huehub'});
+  var brakeBulbQuery = server.where({type: 'huebulb', bulbName:'Carl'});
 
-  server.observe([vehicleQuery, hubQuery], function(vehicle, hub){
-    server.log('App Ready.');
+  server.observe([vehicleQuery, brakeBulbQuery], function(vehicle, hub){
+    var oldData = null;
     vehicle.streams.brake.on('data', function(status){
-      if(status.data) {
-        hub.call('color', '#ff2600');
-      } else {
-        hub.call('color', '#0026ff');
+      if(oldData != status.data) {
+        if(status.data) {
+          hub.call('color', '#ff2600');
+        } else {
+          hub.call('color', '#0026ff');
+        }
       }
+      oldData = status.data;
+    });
+  });
+
+  var accelBulbQuery = server.where({type: 'huebulb', bulbName:'Matt'});
+
+  server.observe([vehicleQuery, accelBulbQuery], function(vehicle, hub){
+    var oldData = null;
+    vehicle.streams.acceleration.on('data', function(status){
+      if(status.data != oldData) {
+        if(status.data > 0) {
+          hub.call('color', '#ff2600');
+        } else {
+          hub.call('color', '#0026ff');
+        }
+      }
+      oldData = status.data;
+    });
+  });
+
+  var wheelBulbQuery = server.where({type: 'huebulb', bulbName:'Adam'});
+
+  server.observe([vehicleQuery, wheelBulbQuery], function(vehicle, hub){
+    var oldData = null;
+    vehicle.streams.wheel.on('data', function(status){
+      if(status.data != oldData) {
+        if(status.data > 0) {
+          hub.call('color', '#ff2600');
+        } else {
+          hub.call('color', '#0026ff');
+        }
+      }
+      oldData = status.data;
     });
   });
 };
