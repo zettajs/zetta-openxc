bad4c7066b60a6235b60e3c2b92905440e41a3fb
Matt Dobson <mdobson4@gmail.com>
2014-08-07 10:37:32 -0400

    Added an interaction for changing hue bulbs when the brake is pressed.

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 module.exports = function(server) {
   var vehicleQuery = server.where({type: 'vehicle'});
+  var hubQuery = server.where({type: 'huehub'});
 
-  server.observe([vehicleQuery], function(vehicle){
-    server.log('Vehicle came online!');
+  server.observe([vehicleQuery, hubQuery], function(vehicle, hub){
+    server.log('App Ready.');
+    vehicle.streams.brake.on('data', function(status){
+      if(status.data) {
+        hub.call('color', '#ff2600');
+      } else {
+        hub.call('color', '#0026ff');
+      }
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var zetta = require('zetta');
 var openxc = require('./index');
 var app = require('./app');
+var Hue = require('zetta-hue-driver');
 
 zetta()
   .use(openxc)
+  .use(Hue)
   .load(app)
   .listen(3000);
