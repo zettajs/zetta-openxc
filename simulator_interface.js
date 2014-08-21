cab3b6430111105124138430e7e651f36fc4c257
Adam Magaluk <AdamMagaluK@gmail.com>
2014-08-21 16:02:43 -0400

    Updates

--- simulator_interface.js
+++ simulator_interface.js
@@ -2,8 +2,7 @@ var net = require('net');
 var EventEmitter = require('events').EventEmitter;
 
 var OpenXC = module.exports = function(cb) {
-  var client = net.connect({port: 50001}, cb);
-
+  var client = net.connect({port: 50001, host: '192.168.1.107'}, cb);
 
   this.emitter = new EventEmitter();
   this.on = this.emitter.on.bind(this);
